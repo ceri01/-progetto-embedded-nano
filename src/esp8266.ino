24afb8a90e6905e6afef2a9b8e3cb8bfabecfc06
Marco Aceti <mail@marcoaceti.it>
2023-05-17 00:20:30 +0200

    Add mqttSend() task

--- src/esp8266.ino
+++ src/esp8266.ino
@@ -21,6 +21,28 @@ const char topic[]  = "baracca/lux-ESP01/";
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
+void mqttSend();
+Task mqttSendTask(5 * TASK_SECOND, TASK_FOREVER, &mqttSend);
+
+Scheduler runner;
+
+void mqttSend() {
+  mqttClient.poll();
+  FirmataClient.digitalWrite(LED, HIGH);
+
+  for (int i = 0; i < 6; i++) {
+    float voltage = (float) FirmataClient.analogRead(i) * (5.0 / 1023);
+
+    mqttClient.beginMessage(topic + String(i));
+    mqttClient.print(voltage);
+    mqttClient.endMessage();
+    delay(50);
+  }
+
+  FirmataClient.digitalWrite(LED, LOW);
+	delay(1000);
+}
+
 void setup() {
 	Serial.begin(57600);
   delay(2000);
@@ -46,26 +68,14 @@ void setup() {
   FirmataClient.pinMode(MOTORE_Y2, OUTPUT);
   FirmataClient.pinMode(MOTORE_Z1_FEEDBACK, INPUT);
   FirmataClient.pinMode(MOTORE_Z2_FEEDBACK, INPUT);
-  
+
+  runner.init();
+  runner.addTask(mqttSendTask);
+  mqttSendTask.enable(); 
 }
 
 void loop() {
   /*
-  mqttClient.poll();
-  FirmataClient.digitalWrite(LED, HIGH);
-
-  for (int i = 0; i < 6; i++) {
-    float voltage = (float) FirmataClient.analogRead(i) * (5.0 / 1023);
-
-    mqttClient.beginMessage(topic + String(i));
-    mqttClient.print(voltage);
-    mqttClient.endMessage();
-    delay(50);
-  }
-
-  FirmataClient.digitalWrite(LED, LOW);
-	delay(1000);*/
-
   FirmataClient.digitalWrite(MOTORE_Z1, HIGH);
   delay(2000);
   FirmataClient.digitalWrite(MOTORE_Z1, LOW);
@@ -78,4 +88,7 @@ void loop() {
   FirmataClient.digitalWrite(MOTORE_Y2, HIGH);
   delay(2000);
   FirmataClient.digitalWrite(MOTORE_Y2, LOW);
+  */
+
+ runner.execute();
 }
