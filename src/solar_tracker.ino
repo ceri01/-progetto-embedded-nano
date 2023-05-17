b7c7404b661af858a71a2b99fe11b7f5eb23d476
Marco Aceti <mail@marcoaceti.it>
2023-05-17 22:57:37 +0200

    Add readSensors, motorMove and executeMovement

--- src/solar_tracker.ino
+++ src/solar_tracker.ino
@@ -1,37 +1,134 @@
 #include <TaskScheduler.h>
 
+#define NORTH_SENSOR A0
+#define SOUTH_SENSOR A3
+#define EAST_SENSOR A1
+#define WEST_SENSOR A2
 #define LED 13
-#define MOTORE_Z1 2
-#define MOTORE_Z2 3
-#define MOTORE_Y1 4
-#define MOTORE_Y2 5
-#define MOTORE_Z1_FEEDBACK 6
-#define MOTORE_Z2_FEEDBACK 7
+
+#define NORTH_SWITCH 2
+#define SOUTH_SWITCH 3
+#define EAST_SWITCH 4
+#define WEST_SWITCH 5
+#define NORTH_LIMIT_SWITCH 6
+#define SOUTH_LIMIT_SWITCH 7
+
+#define EPSILON 1
+#define DELAY 2000
+
+
+/*
+	Struct to describe each sensor value
+*/
+struct brightness {
+	int north;
+	int south;
+	int east;
+	int west;
+};
+
+enum Direction { North = NORTH_SWITCH, South = SOUTH_SWITCH, East = EAST_SWITCH, West = WEST_SWITCH};
+
+/*
+	Read data provided from light sensors.
+	This function return a struct that contains four ints 
+*/
+struct brightness readSensors() {
+	brightness brightnessLevel;
+	brightnessLevel.north = analogRead(NORTH_SENSOR);
+	brightnessLevel.south = analogRead(SOUTH_SENSOR);
+	brightnessLevel.east = analogRead(EAST_SENSOR);
+	brightnessLevel.west = analogRead(WEST_SENSOR);
+	return brightnessLevel;
+}
+
+void executeMovement() {
+	brightness data = readSensors();
+	int vertical = data.north - data.south;
+	int horizontal = data.east - data.west;
+
+	if (vertical > EPSILON) {
+		motorMove(Direction::North, DELAY);
+	} else if (vertical < EPSILON) {
+		motorMove(Direction::South, DELAY);
+	}
+
+	if (horizontal > EPSILON) {
+		motorMove(Direction::East, DELAY);
+	} else if (vertical < EPSILON) {
+		motorMove(Direction::West, DELAY);
+	}
+}
+
+
+/*
+	Move a motor in a given direction for a given time (in milliseconds).
+	Then, return true if the limit switch was activated, false otherwise.
+*/ 
+bool motorMove(Direction direction, int period) {
+	uint8_t digitalPin = static_cast<int>(direction);
+	
+	digitalWrite(digitalPin, HIGH);
+	delay(period);
+
+	bool ret = false;
+	if (direction == Direction::North) {
+		ret = digitalRead(NORTH_LIMIT_SWITCH);
+	} else if (direction == Direction::South) {
+		ret = digitalRead(SOUTH_LIMIT_SWITCH);
+	}
+	digitalWrite(digitalPin, LOW);
+	return ret;
+}
 
 void setup() {
-	Serial.begin(115200);
+	Serial.begin(9600);
 	delay(2000);
 
 	pinMode(LED, OUTPUT);
-	pinMode(MOTORE_Z1, OUTPUT);
-	pinMode(MOTORE_Z2, OUTPUT);
-	pinMode(MOTORE_Y1, OUTPUT);
-	pinMode(MOTORE_Y2, OUTPUT);
-	pinMode(MOTORE_Z1_FEEDBACK, INPUT);
-	pinMode(MOTORE_Z2_FEEDBACK, INPUT);
+	pinMode(NORTH_SWITCH, OUTPUT);
+	pinMode(SOUTH_SWITCH, OUTPUT);
+	pinMode(EAST_SWITCH, OUTPUT);
+	pinMode(WEST_SWITCH, OUTPUT);
+	pinMode(NORTH_LIMIT_SWITCH, INPUT_PULLUP);
+	pinMode(SOUTH_LIMIT_SWITCH, INPUT_PULLUP);
 }
 
 void loop() {
-	digitalWrite(MOTORE_Z1, HIGH);
-	delay(2000);
-	digitalWrite(MOTORE_Z1, LOW);
-	digitalWrite(MOTORE_Z2, HIGH);
-	delay(2000);
-	digitalWrite(MOTORE_Z2, LOW);
-	digitalWrite(MOTORE_Y1, HIGH);
-	delay(2000);
-	digitalWrite(MOTORE_Y1, LOW);
-	digitalWrite(MOTORE_Y2, HIGH);
-	delay(2000);
-	digitalWrite(MOTORE_Y2, LOW);
+	/*
+	bool ret;
+	ret = motorMove(Direction::North, DELAY);
+	Serial.print("N? ");
+	Serial.print(ret);
+
+	ret = motorMove(Direction::South, DELAY);
+	Serial.print("\tS? ");
+	Serial.print(ret);
+
+	ret = motorMove(Direction::East, DELAY);
+	Serial.print("\tE? ");
+	Serial.print(ret);
+
+	ret = motorMove(Direction::West, DELAY);
+	Serial.print("\tW? ");
+	Serial.print(ret);
+
+	Serial.println();
+
+	brightness sensors = readSensors();
+	Serial.print("N=");
+	Serial.print(sensors.north);
+	Serial.print("\tS=");
+	Serial.print(sensors.south);
+	Serial.print("\tE=");
+	Serial.print(sensors.east);
+	Serial.print("\tW=");
+	Serial.print(sensors.west);
+	Serial.println();
+	*/
+
+	while (!motorMove(Direction::South, 1)) {
+
+	}
+	delay(30000);
 }
