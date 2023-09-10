5c35fc5dce1c1a0ac742afc9bc5910830dfddb64
Marco Aceti <mail@marcoaceti.it>
2023-09-10 16:00:19 +0200

    Add MQTT publish values

--- src/Comunication.cpp
+++ src/Comunication.cpp
@@ -23,12 +23,15 @@ void sendData() {
         mqtt.publish(prefix + "south", String(brightness.south));
         mqtt.publish(prefix + "east", String(brightness.east));
         mqtt.publish(prefix + "west", String(brightness.west));
+        mqtt.publish(prefix + "ns_delta", String(brightness.north - brightness.south));
+        mqtt.publish(prefix + "ew_delta", String(brightness.east - brightness.west));
         mqtt.publish(prefix + "north_limit_switch", String(NORTH_LIMIT_REACHED));
         mqtt.publish(prefix + "south_limit_switch", String(SOUTH_LIMIT_REACHED));
         mqtt.publish(prefix + "east_limit_switch", String(EAST_LIMIT_REACHED));
         mqtt.publish(prefix + "west_limit_switch", String(WEST_LIMIT_REACHED));
         mqtt.publish(prefix + "wind", String(RECEIVED_WIND));
         mqtt.publish(prefix + "last_wind_timestamp", String(RECEIVED_WIND_LAST_TIMESTAMP));
+        mqtt.publish(prefix + "last_wind_timestamp_relative", String(millis() - RECEIVED_WIND_LAST_TIMESTAMP));
         mqtt.publish(prefix + "manual_mode", String(MANUAL_MODE));
 }
 #endif
